fb28ffc258e70a9e4adcccb92680be7a93ccdd14
João Gabriel Ferreira Fernandes <jgff157@gmail.com>
2026-08-04 16:26:09 -0300

    Adiciona repositories e ajustes no JPA de locacao e pagamento

--- src/main/java/br/edu/ufape/poo/locadora/negocio/basica/Pagamento.java
+++ src/main/java/br/edu/ufape/poo/locadora/negocio/basica/Pagamento.java
@@ -3,22 +3,38 @@ package br.edu.ufape.poo.locadora.negocio.basica;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public class Pagamento {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
+@Entity
+public class Pagamento {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+	
     private BigDecimal valor;
     private LocalDate dataPagamento;
     private String formaPagamento;
+    
+    @ManyToOne
+    @JoinColumn(name = "locacao_id")
+    private Locacao locacao;
 
     public Pagamento (){
         super ();
     }
 
-    public Pagamento (BigDecimal valor, LocalDate dataPagamento, String formaPagamento){
+    public Pagamento (BigDecimal valor, LocalDate dataPagamento, String formaPagamento, Locacao locacao){
         super ();
         this.valor = valor;
         this.dataPagamento = dataPagamento;
         this.formaPagamento = formaPagamento;
+        this.locacao = locacao;
     }
 
     public Long getId() {
@@ -51,5 +67,28 @@ public class Pagamento {
 
     public void setFormaPagamento(String formaPagamento) {
         this.formaPagamento = formaPagamento;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+    	if (this == o) return true;
+    	if (!(o instanceof Pagamento)) return false;
+    	Pagamento pagamento = (Pagamento) o;
+    	return id != null && id.equals(pagamento.id);
+    }
+    
+    @Override
+    public int hashCode() {
+    	return getClass().hashCode();
+    }
+    
+    @Override
+    public String toString() {
+    	return "Pagamento{" +
+    			"id=" + id +
+    			", valor=" + valor +
+    			", dataPagamento=" + dataPagamento +
+    			", formaPagamento=" + formaPagamento + '\'' +
+    			'}';
     }
 }
