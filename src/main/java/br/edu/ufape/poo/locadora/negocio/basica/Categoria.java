e57de1db892c01752973aab19c0337d3434e3aaf
zlluska <lucasfraga54@gmail.com>
2026-08-04 16:10:40 -0300

    feat:adiciona mapeamento JPA Equals hash nas classes Categoria e Veiculo

--- src/main/java/br/edu/ufape/poo/locadora/negocio/basica/Categoria.java
+++ src/main/java/br/edu/ufape/poo/locadora/negocio/basica/Categoria.java
@@ -1,30 +1,40 @@
 package br.edu.ufape.poo.locadora.negocio.basica;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
 import java.math.BigDecimal;
+import java.util.Objects;
 
+@Entity
+@Table(name = "categoria")
 public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 100)
     private String nome;
+
+    @Column(name = "valor_diaria_base", nullable = false)
     private BigDecimal valorDiariaBase;
 
-    
     public Categoria() {
     }
 
-    
-    public Categoria(final Long id, final String nome, final BigDecimal valorDiariaBase) {
-        this.id = id;
+    public Categoria(final String nome, final BigDecimal valorDiariaBase) {
         this.nome = nome;
         this.valorDiariaBase = valorDiariaBase;
     }
 
-   
     public BigDecimal calcularValorDiaria() {
-        // Lógica para cálculo da diária base 
         return this.valorDiariaBase;
     }
 
-    
     public Long getId() {
         return id;
     }
@@ -47,5 +57,18 @@ public class Categoria {
 
     public void setValorDiariaBase(final BigDecimal valorDiariaBase) {
         this.valorDiariaBase = valorDiariaBase;
+    }
+    
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria)) return false;
+        final Categoria categoria = (Categoria) o;
+        return Objects.equals(nome, categoria.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome);
     }
 }
